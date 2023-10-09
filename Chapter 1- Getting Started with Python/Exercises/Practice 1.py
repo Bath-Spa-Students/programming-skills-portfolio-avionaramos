744b6fd956de950bf91d39ecc4c771bb49f72b2c
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-10-09 09:28:10 +0400

    Revert "Practice 1.py"
    This reverts commit bd04ad90bea7b60d66b8d13d5d7347d20b017d9d.

--- Chapter 1- Getting Started with Python/Exercises/Practice 1.py
+++
@@ -1,5 +0,0 @@
-# Write a program to output the letters W X Y Z, each on a separate line
-
-print("""x
-y
-z""")
--- Chapter 1- Getting Started with Python/Exercises/Practice 2.py
+++
@@ -1,4 +0,0 @@
-# Create  a program to output "hi" 50 times, without any seperator
-
-a = ("hi ")
-print(a*50)

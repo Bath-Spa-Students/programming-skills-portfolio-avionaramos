eb4bf7935aef28e016a157cadba0f9ce449910ee
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-10-12 13:35:01 +0400

    Practice 3.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Practice 3.py
@@ -0,0 +1,5 @@
+#You need to make a program for a leaderboard
+#The program needs to output the numbera 1 to 9, each on a sepearate line followed by a dot
+
+for sheesh in range(1, 10): 
+    print(sheesh, ".")

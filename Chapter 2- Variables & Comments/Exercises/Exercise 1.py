b3cae764773877233261bd9fc2926ed413861480
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-10-20 15:48:21 +0400

    Exercise 1.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 1.py
@@ -0,0 +1,8 @@
+## Assign a message to a variable, and print that message.Then change the value of the variable to a new message, and print the new message.
+
+Ali = "I am hungry"
+print(Ali)
+
+Amara = "I want to eat mango sticky rice"
+print(Amara)
+

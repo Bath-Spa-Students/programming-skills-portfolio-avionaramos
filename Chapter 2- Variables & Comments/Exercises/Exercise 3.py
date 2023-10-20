65d93f3c6f03987029615a7ff285971b20456a6e
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-10-20 15:51:20 +0400

    Exercise 3.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 3.py
@@ -0,0 +1,14 @@
+## Exercise 3: Stripping Names :ballot_box_with_check:
+
+Name = ("\tAmara Tom\n")
+print("Unmodified:" ) 
+print (Name)
+
+print("\nUsinglstrip():")
+print (Name.lstrip()) 
+
+print("\nUsingrstrip():")
+print (Name.rstrip()) 
+
+print("\nUsingstrip():")
+print (Name.strip())  

0dc4feca90e9caceb0415e73dcf614ec706624b5
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-10-20 15:49:05 +0400

    Exercise 2.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 2.py
@@ -0,0 +1,6 @@
+## Exercise 2: Variables :ballot_box_with_check:
+
+quote= ' "The most difficult thing is the decision to act, the rest is merely tenacity."'
+print(f"Will Rogers once said,{quote}")
+
+

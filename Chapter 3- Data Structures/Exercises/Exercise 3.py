b5e930eedb3fca05544f300a39611fab515a7fb6
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-10-30 00:00:00 +0400

    Exercise 3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 3.py
@@ -0,0 +1,10 @@
+cars = ['BMW i7 Electric Sedan', 'Toyota GR Supra', 'Toyota Land Cruiser']
+
+message = f'"I want to experience driving a {cars[0]}."'
+print(message)
+
+message = f'"I want to buy a  {cars[1]}."'
+print(message)
+
+message = f'"I dream of owning a {cars[2]}."'
+print(message)

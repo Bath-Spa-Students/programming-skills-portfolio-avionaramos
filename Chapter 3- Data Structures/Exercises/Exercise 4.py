6305a5de5ea582abcfbe4961b511aeb0eb461dc8
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-10-30 00:00:22 +0400

    Exercise 4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 4.py
@@ -0,0 +1,17 @@
+invitees = ['Alexis', 'Arnie', 'Arniezza', 'Amara', 'Ali']
+
+message1 = (f"Hello, {invitees[0]}. Would you like to come over for dinner tonight?")
+print(message1)
+
+message2 = (f"Hello, {invitees[1]}. Would you like to come over for dinner tonight?")
+print(message2)
+
+message3 = (f"Hello, {invitees[2]}. Would you like to come over for dinner tonight?")
+print(message3)
+
+message4 = (f"Hello, {invitees[3]}. Would you like to come over for dinner tonight?")
+print(message4)
+
+message5 = (f"Hello, {invitees[4]}. Would you like to come over for dinner tonight?")
+print(message5)
+

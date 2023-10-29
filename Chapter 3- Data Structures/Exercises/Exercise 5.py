2660a108b1c22740e9718e084b7bd70c2e225ea9
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-10-30 00:00:37 +0400

    Exercise 5.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 5.py
@@ -0,0 +1,42 @@
+invitees = ['Alexis', 'Arnie', 'Arniezza', 'Amara', 'Ali']
+
+message1 = (f"Hello, {invitees[0]}. Would you like to come over for dinner tonight?")
+print(message1)
+
+message2 = (f"Hello, {invitees[1]}. Would you like to come over for dinner tonight?")
+print(message2)
+
+message3 = (f"Hello, {invitees[2]}. Would you like to come over for dinner tonight?")
+print(message3)
+
+message4 = (f"Hello, {invitees[3]}. Would you like to come over for dinner tonight?")
+print(message4)
+
+message5 = (f"Hello, {invitees[4]}. Would you like to come over for dinner tonight?")
+print(message5)
+
+message4 = f"\nSadly, {invitees[0]} cannot make it tonight."
+print(message4)
+
+del(invitees[0])
+invitees.insert(0, 'Ariza')
+
+message1 = (f"\nHello, {invitees[0]}. Would you like to come over for dinner tonight?")
+print(message1)
+
+message2 = (f"Hello, {invitees[1]}. Would you like to come over for dinner tonight?")
+print(message2)
+
+message3 = (f"Hello, {invitees[2]}. Would you like to come over for dinner tonight?")
+print(message3)
+
+message4 = (f"Hello, {invitees[3]}. Would you like to come over for dinner tonight?")
+print(message4)
+
+message5 = (f"Hello, {invitees[4]}. Would you like to come over for dinner tonight?")
+print(message5)
+
+
+
+
+

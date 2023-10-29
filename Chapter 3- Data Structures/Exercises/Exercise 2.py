b01bd1f00eb2b6a69d54afaedbc938a9cf12ecff
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-10-29 23:59:47 +0400

    Exercise 2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 2.py
@@ -0,0 +1,13 @@
+name = ['Gian', 'Ethan', 'Anne', 'Dan']
+
+message = ("Nice to meet you, " + name[0] + "!")
+print(message)
+
+message = ("Nice to meet you, " + name[1] + "!")
+print(message)
+
+message = ("Nice to meet you, " + name[2] + "!")
+print(message)
+
+message = ("Nice to meet you, " + name[3] + "!")
+print(message)

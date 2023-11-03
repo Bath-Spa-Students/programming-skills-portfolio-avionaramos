ba4c27899777ece2e814f102124d0ced26c9246b
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-11-03 07:18:00 +0400

    Exercise 1.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 1.py
@@ -0,0 +1,12 @@
+## Exercise 1:  Alien Colors #1 :ballot_box_with_check:
+
+## Passed Version
+alien_color = "Green"
+if alien_color == "Green": 
+    print("Well done, you have just earned 5 points!!")
+
+## Failed Version
+alien_color = "Yellow"
+if alien_color == "Green": 
+    print("Well done, you have just earned 5 points!!")
+

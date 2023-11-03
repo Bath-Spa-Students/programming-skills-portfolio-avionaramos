bb2ac752bedd5b9b63a52dd041d523bdd4bc4b96
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-11-03 07:20:52 +0400

    Exercise 7.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 7.py
@@ -0,0 +1,34 @@
+## Exercise 7: Seeing the World :ballot_box_with_check:
+
+places = ['Maldives', 'Norway','Amsterdam', 'Japan', 'Paris']
+
+# print places in original order
+print(places)
+
+print("\nAlphabetical Order: ")
+print(sorted(places))
+
+print("\nOriginal Order: ")
+print(places)
+
+print("\nReverse Alphabetical Order: ")
+print(sorted(places, reverse=True))
+
+print("\nOriginal Order: ")
+print(places)
+
+print("\nReversed Order: ")
+places.reverse()
+print(places)
+
+print("\nOriginal Order: ")
+places.reverse()
+print(places)
+
+print("\nAlphabetical: ")
+places.sort()
+print(places)
+
+print("\nReverse Alphabetical")
+places.sort(reverse=True)
+print(places)

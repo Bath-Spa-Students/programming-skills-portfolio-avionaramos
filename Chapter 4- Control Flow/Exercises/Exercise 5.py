a4ecc345a7117b55b0484b0c3437497205b633e9
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-11-03 07:19:19 +0400

    Exercise 5.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 5.py
@@ -0,0 +1,14 @@
+## Exercise 5: Favorite Fruit :ballot_box_with_check:
+
+favorite_fruits = ['Strawberry', 'Mango', 'Avocado']
+
+if 'Strawberry' in favorite_fruits:
+    print("You really like Strawberry!")
+if 'Pineapple' in favorite_fruits:
+    print("You really like Pineapple!")
+if 'Mango' in favorite_fruits:
+    print("You really like Mango!")
+if 'Kiwi' in favorite_fruits:
+    print("You really like Kiwi!")
+if 'Avocado' in favorite_fruits:
+    print("You really like avocado!")

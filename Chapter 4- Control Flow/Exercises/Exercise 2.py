276e951754e054af9ab728af6d52fc626d07aadc
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-11-03 07:18:18 +0400

    Exercise 2.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 2.py
@@ -0,0 +1,17 @@
+## Exercise 2: Alien Colors #2 :ballot_box_with_check:
+
+## If Block
+alien_color = "Green"
+if alien_color == "Green": 
+    print("Well done, you have just earned 5 points!!")
+else: 
+    print("Well done, you have just earned 10 points!!")
+
+## Else Block
+alien_color = "Yellow"
+if alien_color == "Green": 
+    print("Well done, you have just earned 5 points!!")
+else: 
+    print("Well done, you have just earned 10 points!!")
+
+    

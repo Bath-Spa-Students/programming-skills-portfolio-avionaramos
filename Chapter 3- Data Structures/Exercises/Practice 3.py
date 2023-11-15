691de7a85bbacce9bc4f2f6263468c4a9a908d38
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-11-15 17:13:54 +0400

    Practice 3.py

---
+++ Chapter 3- Data Structures/Exercises/Practice 3.py
@@ -0,0 +1,6 @@
+#Assume names references a list. Write a for loop that displays each element of the list.
+
+Astronomers = ['Einstein', 'Newton', 'Copernicus', 'Kepler']
+
+for names in Astronomers:
+    print(names)

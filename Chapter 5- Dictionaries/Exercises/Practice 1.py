b5628f0417e5bcfcf5f3ac95d7d77f6dbd3e02c7
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-11-15 17:14:55 +0400

    Practice 1.py

---
+++ Chapter 5- Dictionaries/Exercises/Practice 1.py
@@ -0,0 +1,8 @@
+# Use a dictionary to store information about yourself.
+
+information = {'name' : 'Aviona',
+               'surname' : 'Ramos',
+               'age' : '18',
+               'nationality' : 'Philippines',
+               'hobbies' : 'sleeping'}
+print(information)

ceab6b39a0b3147ea4f96f937275c59957df4893
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-11-15 17:14:59 +0400

    Practice 2.py

---
+++ Chapter 5- Dictionaries/Exercises/Practice 2.py
@@ -0,0 +1,9 @@
+#Write a Python program to iterate through the keys of a dictionary and print them.
+
+information = {'name' : 'Aviona',
+               'Surname' : 'Ramos',
+               'Age' : '18',
+               'Nationality' : 'Philippines',
+               'Hobbies' : 'sleeping'}
+for key in information.keys():
+    print(key)

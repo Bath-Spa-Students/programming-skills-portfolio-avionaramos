0d5702d0e02a04512db0b0c948eff58357d08876
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-11-15 17:17:19 +0400

    Practice 1.py

---
+++ Chapter 7- Functions/Exercises/Practice 1.py
@@ -0,0 +1,8 @@
+#Write a Python function that takes two numbers as arguments and returns their sum.
+
+def two_numbers(x, y):
+    return x + y
+number1 = 9
+number2 = 11
+outcome = two_numbers(number1, number2)
+print(outcome)

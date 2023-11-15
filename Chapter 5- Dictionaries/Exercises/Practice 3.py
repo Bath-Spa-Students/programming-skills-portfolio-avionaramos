525d8e37cfc964d81b9a39c376f44c23b63d7aa0
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-11-15 17:16:19 +0400

    Practice 3.py

---
+++ Chapter 5- Dictionaries/Exercises/Practice 3.py
@@ -0,0 +1,9 @@
+# Write a Python program to iterate through the values of a dictionary and print them.
+
+information = {'name' : 'Aviona',
+               'Surname' : 'Ramos',
+               'Age' : '18',
+               'Nationality' : 'Philippines',
+               'Hobbies' : 'sleeping'}
+for value in information.values():
+    print(value)

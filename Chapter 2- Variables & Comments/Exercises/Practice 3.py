0c3a7548462b3f8aba3a767805196829ab21bf4a
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-11-15 17:12:36 +0400

    Practice 3.py

---
+++ Chapter 2- Variables & Comments/Exercises/Practice 3.py
@@ -0,0 +1,14 @@
+#Write a python program that takes an input 5 from user and then type cast those values to string, int
+#and float in the separate variables. Print all the variables.
+
+user = (input("Enter a value: "))
+
+string = str(user)
+print("String:", string)
+
+integer = int(user)
+print("Integer:", integer)
+
+float = float(user)
+print("Float:", float)
+

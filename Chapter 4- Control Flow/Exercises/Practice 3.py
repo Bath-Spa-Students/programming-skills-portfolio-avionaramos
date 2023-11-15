8380c679f87af03c83244da763e1b39fe9c6cf90
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-11-15 17:14:27 +0400

    Practice 3.py

---
+++ Chapter 4- Control Flow/Exercises/Practice 3.py
@@ -0,0 +1,12 @@
+#Write a python program with nested decision structures that perform the following: If amount1
+#is greater than 10 and amount2 is less than 100, display the greater of amount1 and amount2
+
+amount1 = 20
+amount2 = 50
+
+if amount1 > amount2: 
+    print("Greater amount is:", amount1)
+elif amount2 > amount1:
+    print("Greater amount is:", amount2)
+else: 
+    print("amount1 and amount2 are equal.")

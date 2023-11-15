74fe57acb75de3a4472abcc2deee6a3373420aa4
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-11-15 17:14:17 +0400

    Practice 1.py

---
+++ Chapter 4- Control Flow/Exercises/Practice 1.py
@@ -0,0 +1,14 @@
+#Write a python program with if statement that assigns 20 to the variable y and assigns 40 to the
+#variable z if the variable & is greater than 100.
+
+number = 200
+x = 0
+y = 0
+
+if number > 100:
+    y = 20
+    x = 40
+
+print("Y:", y)
+print("X:", x)
+

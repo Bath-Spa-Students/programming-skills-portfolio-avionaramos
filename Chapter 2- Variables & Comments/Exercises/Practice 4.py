4a394b75b7109c17550a40ce4ac57fcfc96d3bf9
avionaramos <144330380+avionaramos@users.noreply.github.com>
2023-11-15 17:12:45 +0400

    Practice 3.py

---
+++ Chapter 2- Variables & Comments/Exercises/Practice 4.py
@@ -0,0 +1,8 @@
+#Write a python program that stores an integer and string value to variables x and y. Print the type of 
+#variable x and y. 
+
+x = 20
+y = "Good Morning!"
+
+print("The type of x: ",type(x))
+print("The type of y: ",type(y))
